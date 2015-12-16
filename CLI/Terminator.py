28cb542cf912b7a018af349bac682fd340eaa7ae
Geoffrey Lentner <glentner@nd.edu>
2015-12-16 12:24:35 -0500

    refactoring

--- CLI/Terminator.py
+++ CLI/Terminator.py
@@ -4,7 +4,8 @@
 
 """Implementation of Terminator(Argument)."""
 
-from .Argument import Argument
+from .Argument   import Argument
+from .Exceptions import Error
 
 class Terminator(Argument):
     """
@@ -15,5 +16,27 @@ class Terminator(Argument):
 
     def __init__(self, description, information, short=None):
         """Initialize the new Terminator(Argument)."""
-        self.infomation = str(information)
         super(Terminator, self).__init__(description, False, short)
+        self.information = information
+
+
+    def help(self, spacing = 10):
+        """Return the *help* string for this *Argument*."""
+        if not self.short:
+            return " --{}{}{} (default: {}).\n".format(self.name, " " *
+                    (spacing - len(self.name) - 2), self.description, self.default)
+        else:
+            return " -{}, --{}{}{} (default: {}).\n".format(self.short, self.name,
+                    " " * (spacing - len(self.name) - 6), self.description, self.default)
+
+
+    def set(self, value):
+        """Specialized method for setting the *value* of the Flag."""
+        if type(value) is bool:
+            self.value = value
+        elif type(value) is str:
+            if value.strip() not in ["0", "1"]:
+                raise Error("The Flag(Argument) `{}`: can only take '0' or '1'!".format(self.name))
+        else:
+            # the behavior is dictated by coercion (be careful!)
+            self.value = bool(value)
